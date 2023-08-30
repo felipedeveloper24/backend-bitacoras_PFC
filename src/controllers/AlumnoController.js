acce257448b570cc450c658e8157db72b5926bde
felipedeveloper24 <felipe.developer24@gmail.com>
2023-08-29 23:52:37 -0400

    update conocimientos

--- src/controllers/AlumnoController.js
+++ src/controllers/AlumnoController.js
@@ -30,21 +30,24 @@ const obtener_alumno = async(req,res)=>{
         })
     }
 }
-
+/*
 const obtener_apitudes_alumno =  async(req,res)=>{
     try{
         const {id_alumno} = req.body;
         const aptitudes = await prisma.conocimiento.findMany({
+            
             where:{
                 id_alumno:Number(id_alumno)
             },
             include:{aptitud:true}
+            
         })
         if(aptitudes.length==0){
             return res.status(200).json({
                 mensaje:"No tiene aptitudes asociadas"
             })
         }
+        
         return res.status(200).json({
             mensaje:"Se han encontrado aptitudes",
             aptitudes:aptitudes
@@ -56,5 +59,40 @@ const obtener_apitudes_alumno =  async(req,res)=>{
         })
     }
 }
+*/
+const obtener_apitudes_alumno =  async(req,res)=>{
+    try{
+        const {id_alumno} = req.body;
+        const aptitudes = await prisma.conocimiento.findMany({
+            where:{
+                id_alumno:Number(id_alumno)
+            },
+            include:{aptitud:true}
+        });
+
+        if(aptitudes.length==0){
+            return res.status(200).json({
+                mensaje:"No tiene aptitudes asociadas"
+            });
+        }
+
+        // Ordenando las aptitudes alfabéticamente según el campo 'nombre' (o cualquier otro campo que quieras)
+        aptitudes.sort((a, b) => {
+            if ( a.aptitud.nombre_aptitud < b.aptitud.nombre_aptitud) return -1;
+            if (a.aptitud.nombre_aptitud > b.aptitud.nombre_aptitud) return 1;
+            return 0;
+        });
+
+        return res.status(200).json({
+            mensaje:"Se han encontrado aptitudes",
+            aptitudes:aptitudes
+        });
+
+    } catch(error) {
+        return res.status(400).json({
+            mensaje:"error al encontrar aptitudes"
+        });
+    }
+};
 
 module.exports={obtener_alumno,obtener_apitudes_alumno};
